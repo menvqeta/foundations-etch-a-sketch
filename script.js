c2aaa871c3e1b0f45b57624df829b1e62fab9e55
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-13 20:18:29 +0530

    Added script 2

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-gridSizeSlider = document.querySelector("#grid-size-slider");
-sliderText = document.querySelector(".slider-text");
-sketchPadSquare = document.querySelector(".sketch-pad-square");
+const gridSizeSlider = document.querySelector("#grid-size-slider");
+const sliderText = document.querySelector(".slider-text");
+const sketchPadSquare = document.querySelector(".sketch-pad-square");
+const blackButton = document.querySelector("#black-button");
 
-function adjustSlider() {
-    let lengthOfGridSide = gridSizeSlider.value;
+function adjustSlider(lengthOfGridSide) {
     sliderText.textContent = `Grid size: ${lengthOfGridSide}x${lengthOfGridSide}`;
     sketchPadSquare.innerHTML = '';
     const flexBasisValue = `calc(100% / ${lengthOfGridSide})`;
-    for(let i=0; i<lengthOfGridSide; i++) {
+    addSquares(lengthOfGridSide, flexBasisValue);
+}
+
+function addSquares(lengthOfGridSide, flexBasisValue) {
+    for (let i = 0; i < lengthOfGridSide; i++) {
         var row = document.createElement('div');
         row.setAttribute('class', 'row');
-        for(let j=0; j<lengthOfGridSide; j++) {
+        for (let j = 0; j < lengthOfGridSide; j++) {
             var smallBox = document.createElement('div');
             smallBox.setAttribute('class', 'small-box');
             smallBox.style.flexBasis = flexBasisValue;
-            console.log(smallBox);
+            smallBox.addEventListener('mouseover', ()=> colorSquareOnHovering(smallBox));
             row.appendChild(smallBox);
         }
         sketchPadSquare.appendChild(row);
     }
 }
 
-gridSizeSlider.addEventListener('input', ()=> adjustSlider());
+function colorSquareOnHovering(smallBox) {  
+    smallBox.style.backgroundColor  = 'red';
+    console.log(smallBox);
+}
+
+gridSizeSlider.addEventListener('input', ()=> adjustSlider(gridSizeSlider.value));
+
+
