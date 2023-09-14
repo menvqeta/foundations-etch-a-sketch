949994b9f3fceb0e718fe713c5b82e2060a1217c
menvqeta <34645476+menvqeta@users.noreply.github.com>
2023-09-14 19:58:55 +0530

    Added script  4

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const blackButton = document.querySelector("#black-button");
 const rgbButton = document.querySelector("#rgb-button");
 const eraserButton = document.querySelector("#eraser-button");
 const resetButton = document.querySelector("#reset-button");
-let currentColor = 'black';
+const footerText = document.querySelector(".footer-text");
+let lastClickedButton = 'black';
 
 function adjustSlider(lengthOfGridSide) {
     sliderText.textContent = `Grid size: ${lengthOfGridSide}x${lengthOfGridSide}`;
@@ -34,19 +35,32 @@ function addSquares(lengthOfGridSide, flexBasisValue) {
 }
 
 function colorSquareOnHovering(smallBox) {  
-    smallBox.style.backgroundColor  = currentColor;
+    if(lastClickedButton === 'black') {
+        smallBox.style.backgroundColor = 'black';
+    }
+    else if(lastClickedButton === 'eraser') {
+        smallBox.style.backgroundColor = 'white';
+    }
+    else if(lastClickedButton === 'random') {
+        smallBox.style.backgroundColor = getRandomRbgColor();
+    }
 }
 
-function changeColor(color) {
-    currentColor = color;
+function changeLastClickedButton(color) {
+    lastClickedButton = color;
 }
 
-gridSizeSlider.addEventListener('input', ()=> adjustSlider(gridSizeSlider.value));
-blackButton.addEventListener('click', ()=> changeColor('black'));
-eraserButton.addEventListener('click', ()=> changeColor('white'));
-rgbButton.addEventListener('click', ()=> changeColor(getRandomRbgColor()));
+function resetAllBoxes() {
+    let smallBoxes = document.querySelectorAll(".small-box");
+    for(let i=0; i<smallBoxes.length; i++) {
+        let smallBox = smallBoxes[i];
+        smallBox.style.backgroundColor = 'white';
+    }
+}
 
-const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+function randomBetween(min, max) {
+   return min + Math.floor(Math.random() * (max - min + 1));
+}
 
 function getRandomRbgColor() {
     const r = randomBetween(0, 255);
@@ -56,7 +70,26 @@ function getRandomRbgColor() {
     return rgb;
 }
 
+function addEventListeners() {
+    gridSizeSlider.addEventListener('input', () => adjustSlider(gridSizeSlider.value));
+    blackButton.addEventListener('click', () => changeLastClickedButton('black'));
+    eraserButton.addEventListener('click', () => changeLastClickedButton('eraser'));
+    rgbButton.addEventListener('click', () => changeLastClickedButton('random'));
+    resetButton.addEventListener('click', () => resetAllBoxes());
+}
+
+function updateFooterText() {
+    let currentYear =  new Date().getFullYear();
+    footerText.textContent = `Copyright © ${currentYear} menvqeta`;
+}
+
+addEventListeners();
+
 
 adjustSlider(2);
+
+updateFooterText();
+
+
 
 
